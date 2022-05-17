c11ba0663b2a3455042e5cfc66cd1a55f0de7a01
Allan Coutarel <78037218+DevNemoo@users.noreply.github.com>
2022-05-17 18:29:06 +0200

    cleaning files

--- build-WifiBot-Desktop_x86_windows_msvc2019_pe_64bit-Debug/qtcsettings.cmake
+++
@@ -1,9 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "D:/Logiciels/QT_Creator/Tools/QtCreator/share/qtcreator/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "D:/Logiciels/QT_Creator/5.15.2/msvc2019_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "D:/Logiciels/QT_Creator/5.15.2/msvc2019_64" CACHE "PATH" "" FORCE)
-set("CMAKE_C_COMPILER" "D:/Logiciels/Microsoft Visual Studio/2019/BuildTools/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "D:/Logiciels/Microsoft Visual Studio/2019/BuildTools/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
